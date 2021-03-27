e077a3136d2b3df806d8cd27be2b9e7344c78c52
satdeputr <80572135+satdeputr@users.noreply.github.com>
2021-03-27 20:03:25 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:exchangerates/exchange_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kurs Mata Uang',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ExchangePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
